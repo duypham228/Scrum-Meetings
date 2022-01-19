ec0e3ddfe68b52f2a92d8b76de910ca88ce6eed9
duypham228 <duy.cs.pham@gmail.com>
2022-01-19 09:51:28 -0600

    Initial Commit

---
+++ Scrum-Meetings/Scrum_MeetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scrum_MeetingsApp.swift
+//  Scrum-Meetings
+//
+//  Created by Duy Pham on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Scrum_MeetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
